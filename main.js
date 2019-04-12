af51259e382165d111f308a9716aaa1da956955f
Katherine Williams <kawilliams8@gmail.com>
2019-04-12 08:12:10 -0600

    Initialize JS
    Add first global variables

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 //Global variables
-
+titleInput = element.querySelector(".sidebar__form1-title");
+itemInput = element.querySelector(".sidebar__form1-item");
+makeListButton = element.querySelector(".sidebar__form1-make");
+clearAllButton = element.querySelector(".sidebar__form1-clear");
+taskCollection = [];
 
 //Event Listeners
+
 
 
 //Function declarations
