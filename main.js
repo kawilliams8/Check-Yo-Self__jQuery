4bdb87fa4f241bf21e7de82a10ca7f6e9827dc94
Katie Williams <kawilliams8@gmail.com>
2019-04-12 11:00:04 -0600

    Add normalize CSS file and subsequent adjustments

--- main.js
+++ main.js
@@ -3,10 +3,31 @@ titleInput = element.querySelector(".sidebar__form1-title");
 itemInput = element.querySelector(".sidebar__form1-item");
 makeListButton = element.querySelector(".sidebar__form1-make");
 clearAllButton = element.querySelector(".sidebar__form1-clear");
-taskCollection = [];
+var taskCollection;
 
 //Event Listeners
+window.addEventListener('load', loadPage);
 
+titleInput.addEventListener('keyup', enableListButton);
+itemInput.addEventListener('keyup', enableListButton);
+
+makeListButton.addEventListener('click', makeList)
 
 
 //Function declarations
+function loadPage() {
+  makeListButton.disabled = true;
+  enableListButton();
+}
+
+function enableListButton() {
+    if (titleInput.value === "" || itemInput.value === "") {
+        makeListButton.disabled = true;
+    } else {
+        makeListButton.disabled = false;
+    }
+}
+
+function makeList() {
+
+}
