13c9b355f47405e06f90b9abc8ec833a5f9c2c66
Katie Williams <kawilliams8@gmail.com>
2019-04-16 15:03:53 -0600

    Update Class file for style guide compliance

--- main.js
+++ main.js
@@ -32,6 +32,7 @@ function reinstantiateToDos(toDoCollection) {
     var newData = new ToDo(data.id, data.title, data.task, data.urgent, data.completed);
     return newData;
   })
+
   displaySavedToDos(newToDoInstances);
 }
 
@@ -72,6 +73,7 @@ function collectTaskList(toDoInstance, toDoCard) {
 				<p class="todo__middle-text">${data.text}</p>
 		</div>`
   })
+
   return cardTasks;
 }
 
@@ -95,8 +97,10 @@ function addTaskToCollection(newTask) {
       id: newTaskArray[i].dataset.id,
       checked: false
     }
+
     secondTaskArray.push(taskObj);
   }
+  
   instantiateToDo(secondTaskArray);
 }
 
--- todo-list.js
+++ todo-list.js
@@ -1,31 +1,28 @@
 class ToDo {
-    constructor(id, title, taskObj, urgent) {
-        this.id = id;
-        this.title = title;
-        this.task = taskObj || [];
-        this.urgent = urgent || false;
-    }
+  constructor(id, title, taskObj, urgent) {
+    this.id = id;
+    this.title = title;
+    this.task = taskObj || [];
+    this.urgent = urgent || false;
+  }
 
-    saveToStorage(toDoCollection) {
-        localStorage.setItem("savedTodos", JSON.stringify(toDoCollection));
-    }
+  saveToStorage(toDoCollection) {
+    localStorage.setItem("savedTodos", JSON.stringify(toDoCollection));
+  }
 
-    deleteFromStorage(index) {
-        console.log("hi");
-        toDoCollection.splice(index, 1);
-        this.saveToStorage();
-    }
+  deleteFromStorage(index) {
+    toDoCollection.splice(index, 1);
+      this.saveToStorage();
+  }
 
-    updateToDo() {
-        //for title, tasks and urgency
-        // this.urgent = !this.urgent;
-        this.saveToStorage();
-    }
+  updateToDo() {
+    // this.urgent = !this.urgent;
+    this.saveToStorage();
+  }
  
-    updateTask(editedTitle, completed) {
-        //to update content and if completed (T/F)
-        //this.title = editedTitle;
-        //this.completed = completed || false;
-        this.saveToStorage();
-    }
+  updateTask(editedTitle, completed) {
+    //this.completed = completed || false;
+    //this.title = editedTitle;
+    this.saveToStorage();
+  }
 }
